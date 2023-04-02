1090119c2a8b9b8a64936a22917730d4161e8ac4
nelcar <nelsonjc.01@gmail.com>
2023-04-02 14:21:07 -0600

    Send tweets from json file #5

--- src/app/components/tweet/tweet.component.ts
+++ src/app/components/tweet/tweet.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { TwitterService } from 'src/app/services/twitter.service';
 
 @Component({
@@ -6,24 +7,32 @@ import { TwitterService } from 'src/app/services/twitter.service';
   templateUrl: './tweet.component.html',
   styleUrls: ['./tweet.component.scss'],
 })
-export class TweetComponent {
-  status = '';
+export class TweetComponent implements OnInit {
+  tweets: any[] = [];
+  publishedTweets: any[] = [];
 
-  constructor(private twitterService: TwitterService) {}
+  constructor(private twitterService: TwitterService, private http: HttpClient) { }
 
-  async onSubmit() {
-    if (this.status) {
-      try {
-        const response = await this.twitterService.postTweet(this.status);
-        console.log(response);
-        alert('Tweet publicado exitosamente');
-        this.status = '';
-      } catch (error) {
-        console.error(error);
-        alert('Hubo un error al publicar el tweet');
-      }
-    } else {
-      alert('Por favor, escribe un tweet antes de enviar');
+  async ngOnInit() {
+    const response = await this.http.get<any[]>('assets/tweets.json').toPromise();
+    if (response) {
+      this.tweets = response;
+    }
+  
+    const lastTweetId = localStorage.getItem('lastTweetId');
+    if (lastTweetId) {
+      this.tweets = this.tweets.filter(tweet => tweet.id > +lastTweetId);
     }
   }
+  
+
+  async postTweets() {
+    for (const tweet of this.tweets) {
+      await this.twitterService.postTweet(tweet.tweet);
+      this.publishedTweets.push(tweet);
+      localStorage.setItem('lastTweetId', tweet.id.toString());
+      await new Promise(resolve => setTimeout(resolve, 25 * 60 * 1000));
+    }
+  }
+  
 }
